4abf8b86e511b518bf70ce39dfd216d41e75256f
Matthew <mtac002@outlook.com>
2014-10-14 23:24:57 -0400

    Updated to new FTB relase
    just as it says

--- SKCLauncher Package Manifest/build.sh
+++
@@ -1,9 +0,0 @@
-java -cp SKCraftLauncher.jar com.skcraft.launcher.builder.PackageBuilder
-  --version "0.1"
-  --manifest-dest "dist/manifest.json"
-  --objects-dest "dist/objects"
-  --files "src"
-  --config "modpack.json"
-  --objects-url "objects"
-  --version-file "version.json"
-  --libs-url "http://hostilethon.com/libraries/"
